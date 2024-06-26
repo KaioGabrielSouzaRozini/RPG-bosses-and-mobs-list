2f609839443c6450f25765e099152a149ed8c487
Kaio <kaiogabrielrozini@gmail.com>
2024-06-26 20:05:37 -0300

    iniciando o projeto de lista de mobs e bosses do meu rpg

---
+++ kaio/src/main/java/RPG_bosses_and_mobs/kaio/KaioApplication.java
@@ -0,0 +1,13 @@
+package RPG_bosses_and_mobs.kaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaioApplication.class, args);
+	}
+
+}
---
+++ kaio/src/test/java/RPG_bosses_and_mobs/kaio/KaioApplicationTests.java
@@ -0,0 +1,13 @@
+package RPG_bosses_and_mobs.kaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
