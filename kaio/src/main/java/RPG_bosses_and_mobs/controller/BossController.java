521878ff14af7a93e85e6237e964f76c9fbdc6f4
Kaio <kaiogabrielrozini@gmail.com>
2024-07-01 21:20:16 -0300

    tentando ceoncertar

--- kaio/src/main/java/RPG_bosses_and_mobs/controller/BossController.java
+++ kaio/src/main/java/RPG_bosses_and_mobs/controller/BossController.java
@@ -2,6 +2,7 @@ package RPG_bosses_and_mobs.controller;
 
 import RPG_bosses_and_mobs.model.Boss;
 import RPG_bosses_and_mobs.service.BossService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 public class BossController {
     private final BossService bossService;
 
+    @Autowired
     public BossController(BossService bossService) {
         this.bossService = bossService;
     }
--- kaio/src/main/java/RPG_bosses_and_mobs/service/BossService.java
+++ kaio/src/main/java/RPG_bosses_and_mobs/service/BossService.java
@@ -2,11 +2,15 @@ package RPG_bosses_and_mobs.service;
 
 import RPG_bosses_and_mobs.model.Boss;
 import RPG_bosses_and_mobs.repo.BossRepo;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
+@Transactional
 public class BossService {
     private final BossRepo bossRepo;
 
